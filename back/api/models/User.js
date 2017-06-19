6767961b0bab315621c0a7be1939df578460202e
Martin Markovic <martin.markovic@gauss.hr>
2017-06-19 14:53:02 +0200

    aaa

---
+++ back/api/models/User.js
@@ -0,0 +1,20 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  schema: true,
+
+  attributes: {
+
+    fName : { type: 'text' },
+    lName : { type: 'text' },
+    password: { type: 'string'},
+    
+
+  }
+};
+
