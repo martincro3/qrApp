ffa512cdbef420649b3cb824a5b091696b94848e
Martin Markovic <martin.markovic@gauss.hr>
2017-07-17 15:43:08 +0200

    vracanej na staro

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Http, Response,Headers} from '@angular/http';
 import  'rxjs/Rx';
-import {Router} from '@angular/router';
+
 
 
 
@@ -14,13 +14,13 @@ export class LoginComponent  {
 
   constructor(
       private _http: Http,
-      private router: Router
+      
   )
       { }
   username:string;
   password:string;
   message:any;
-  isAdmin:any;
+  
 
   ngOnInit() {
   }
@@ -29,10 +29,7 @@ export class LoginComponent  {
   return this._http.post('http://localhost:1337/login', {username,password})
                    .map((res: Response)=> {
                      this.message = res.text()
-                     this.isAdmin = res.json().isAdmin
-                     if (this.isAdmin===true){
-                       this.router.navigate(['/admin'])
-                     }
+                     
                     })
                    .subscribe(result => console.log(result))
 }
